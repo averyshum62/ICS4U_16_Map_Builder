c9312c1aba65bc60395df1bbab3c5888ba42a1d0
Marco Yang <marcoyang123@gmail.com>
2017-11-24 13:31:32 -0500

    package Play

--- GamePanel.java
+++ GamePanel.java
@@ -1,32 +1,78 @@
 package Play;
 
-import Game.GMap;
+import Game.Map;
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.awt.event.*;
+import java.io.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
-    private GMap map;
+    private Map map;
 
     public GamePanel() {
-        try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("res/ics.map"));
-            map = (GMap) in.readObject();
-            in.close();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
+        map = new Map();
+        this.setPreferredSize(new Dimension(640, 640));
     }
 
-    public void setMap(GMap map) {
+    public void setMap(Map map) {
         this.map = map;
         repaint();
+        setFocusable(true);
+        requestFocusInWindow();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         map.draw(g);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("LOAD")) {
+            JFileChooser fc = new JFileChooser();
+            fc.setCurrentDirectory(new File("save"));
+
+            try {
+                if (fc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                    ObjectInputStream in = new ObjectInputStream(new FileInputStream(fc.getSelectedFile()));
+                    Map load = (Map) in.readObject();
+                    this.setMap(load);
+                    in.close();
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Pass
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            map.movePlayerUp(true);
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            map.movePlayerLeft(true);
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            map.movePlayerRight(true);
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            map.movePlayerDown(true);
+
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            map.movePlayerUp(false);
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            map.movePlayerLeft(false);
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            map.movePlayerRight(false);
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            map.movePlayerDown(false);
     }
 }
--- PlayFrame.java
+++ PlayFrame.java
@@ -9,17 +9,47 @@ public class PlayFrame extends JFrame {
     public PlayFrame(String title) {
         super(title);
 
+        // Reference to content pane
         Container cont = this.getContentPane();
 
-        GamePanel gp = new GamePanel();
+        /* Declare JPanels and JButtons */
+        GamePanel gp;
+        JPanel pGPWrapper, pHUD;
+        JButton bLoad;
+        /* End declaration*/
 
-        JPanel pHUD = new JPanel();
-        pHUD.setLayout(new FlowLayout());
-        JLabel foo = new JLabel("Hello");
-        pHUD.add(foo);
+        /* Initialise JPanels */
+        gp = new GamePanel();
+        pGPWrapper = new JPanel();
+        pHUD = new JPanel();
+        /* End init */
 
+        /* GamePanel */
+        gp.addKeyListener(gp);
+        pGPWrapper.setLayout(new GridBagLayout());
+        pGPWrapper.add(gp);
+        pGPWrapper.setPreferredSize(gp.getPreferredSize());
+        /* End GamePanel */
+
+        /* HUD */
+        pHUD.setLayout(new BoxLayout(pHUD, BoxLayout.Y_AXIS));
+
+        pHUD.add(Box.createVerticalStrut(20));
+
+        bLoad = new JButton("Load", Images.getImageIcon(Constants.LOAD));
+        bLoad.setFont(new Font("Arial", Font.BOLD, 32));
+        bLoad.setIconTextGap(10);
+        bLoad.setBackground(new Color(230, 230, 230));
+        bLoad.addActionListener(gp);
+        bLoad.setActionCommand("LOAD");
+        pHUD.add(bLoad);
+
+        pHUD.setPreferredSize(new Dimension(450, cont.getHeight()));
+        /* End HUD */
+
+        // Add components to content pane
         cont.setLayout(new BorderLayout());
-        cont.add(gp, BorderLayout.CENTER);
+        cont.add(pGPWrapper, BorderLayout.CENTER);
         cont.add(pHUD, BorderLayout.EAST);
 
         // Set window options
@@ -28,11 +58,9 @@ public class PlayFrame extends JFrame {
     }
 
     public static void main(String[] args) {
-        GImages.load();
+        Images.load();
 
         PlayFrame frame = new PlayFrame("Maze Game");
         frame.setVisible(true);
     }
-
-    //todo: load
 }
